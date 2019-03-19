506f4ed7b71b41884f40e9911db5352882df088f
Raymond Dasilva <rdasilva@fieldedge.com>
2019-03-19 16:07:23 -0400

    New Static Test Case

---
+++ SeleniumProject/TestCase/Static Tests/FE-5227_Web Static Screen Test Plan for Phone Setup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.TestCase.Static_Tests
+{
+    class FE_5227_Web_Static_Screen_Test_Plan_for_Phone_Setup
+    {
+    }
+}
