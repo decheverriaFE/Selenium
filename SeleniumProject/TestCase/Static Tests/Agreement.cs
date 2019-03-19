f307ffba58531c5959d2e4bd9269afc76deecf1e
Delvis Echeverria <decheverria@fieldedge.com>
2019-03-19 14:01:31 -0400

    Update1

---
+++ SeleniumProject/TestCase/Static Tests/Agreement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.TestCase.Static_Tests
+{
+    class Agreement
+    {
+    }
+}
