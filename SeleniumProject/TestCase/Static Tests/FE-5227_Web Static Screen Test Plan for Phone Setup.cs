cda3aeab474998af82a0c74ca2d2407d57c0126c
Raymond Dasilva <rdasilva@fieldedge.com>
2019-03-20 09:43:50 -0400

    Initial setup

--- SeleniumProject/TestCase/Static Tests/FE-5227_Web Static Screen Test Plan for Phone Setup.cs
+++ SeleniumProject/TestCase/Static Tests/FE-5227_Web Static Screen Test Plan for Phone Setup.cs
@@ -28,6 +28,7 @@ namespace SeleniumProject.TestCase.Static_Tests
 
             //Handle popups
             WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#wm-shoutout-144685 > div.wm-close-button.walkme-x-button")));
             IWebElement CloseWalkme1 = Driver.FindElement(By.CssSelector("#wm-shoutout-144685 > div.wm-close-button.walkme-x-button"));
             CloseWalkme1.Click();
@@ -54,6 +55,12 @@ namespace SeleniumProject.TestCase.Static_Tests
 
 
             await Task.Delay(5000);
+        }
+
+        [TearDown]
+        public void Closer()
+        {
+            Driver.Close();
         }
     }
 
