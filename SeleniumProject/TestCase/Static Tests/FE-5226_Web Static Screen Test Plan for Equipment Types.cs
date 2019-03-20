9369f0364f4504da63ef29f7496989c4ed73f1e5
Raymond Dasilva <rdasilva@fieldedge.com>
2019-03-20 16:08:41 -0400

    Fixes -

---
+++ SeleniumProject/TestCase/Static Tests/FE-5226_Web Static Screen Test Plan for Equipment Types.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumProject.Handler;
+using SeleniumProject.PageObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumWaitHelper = SeleniumExtras.WaitHelpers;
+using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
+using NUnit.Framework;
+namespace SeleniumProject.TestCase.Static_Tests
+{
+    class FE_5226_Web_Static_Screen_Test_Plan_for_Equipment_Types
+    {
+    }
+}
