2dff42392e79f1cbcec26e763b0f129832ae62b4
Raymond Dasilva <rdasilva@fieldedge.com>
2019-03-21 08:58:10 -0400

    New: Equipment Static

--- SeleniumProject/TestCase/Static Tests/FE-5226_Web Static Screen Test Plan for Equipment Types.cs
+++ SeleniumProject/TestCase/Static Tests/FE-5226_Web Static Screen Test Plan for Equipment Types.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumProject.Handler;
 using SeleniumProject.PageObject;
@@ -12,9 +14,25 @@ using SeleniumWaitHelper = SeleniumExtras.WaitHelpers;
 using System.Windows.Forms;
 using Keys = OpenQA.Selenium.Keys;
 using NUnit.Framework;
+
 namespace SeleniumProject.TestCase.Static_Tests
 {
     class FE_5226_Web_Static_Screen_Test_Plan_for_Equipment_Types
     {
+        IWebDriver Driver = new ChromeDriver;
+
+        [SetUp]
+        public async Task Initialize()
+        {
+            HomePage home = new HomePage(Driver);
+            home.gotoPage();
+            await Task.Delay(8000);
+        }
+
+        [Test, Order(1)]
+        public async Task EquipmentTypes()
+        {
+
+        }
     }
 }
