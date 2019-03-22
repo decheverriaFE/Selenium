96f4751e4c88c47f70ab594b38f21768229acf48
Raymond Dasilva <rdasilva@fieldedge.com>
2019-03-22 08:07:44 -0400

    Added FE-5225

---
+++ SeleniumProject/TestCase/Static Tests/FE-5225_Web Static Screen Test Plan for Item Screen.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumProject.Handler;
+using SeleniumProject.PageObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumWaitHelper = SeleniumExtras.WaitHelpers;
+using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
+using NUnit.Framework;
+
+namespace SeleniumProject.TestCase.Static_Tests
+{
+    class FE_5225_Web_Static_Screen_Test_Plan_for_Item_Screen
+    {
+        IWebDriver Driver = new ChromeDriver();
+
+        [SetUp]
+        public async Task Initialize()
+        {
+            HomePage home = new HomePage(Driver);
+            home.gotoPage();
+            await Task.Delay(8000);
+        }
+
+        [Test, Order(1)]
+        public async Task ItemsScreen()
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+
+            await Task.Delay(8000);
+        }
+
+
+    }
+}
