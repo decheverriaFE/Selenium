5783a5f2803a778f1e62c745b8e45ab59720a60a
Raymond Dasilva <rdasilva@fieldedge.com>
2019-03-25 10:59:37 -0400

    Added Handlers - Will need to create

---
+++ SeleniumProject/Handler/ReportHandler.cs
@@ -0,0 +1,27 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using NUnit.Framework;
+using System;
+
+namespace AutomationFramework.Handler
+{
+    public sealed class ReportHandler
+    {
+        private static readonly Lazy<ExtentReports> Lazy = new Lazy<ExtentReports>(() => new ExtentReports());
+
+        public static ExtentReports Instance { get { return Lazy.Value; } }
+
+        static ReportHandler()
+        {
+            var htmlReporter = new ExtentHtmlReporter(TestContext.CurrentContext.TestDirectory + "\\Report.html");
+            htmlReporter.LoadConfig(TestContext.CurrentContext.TestDirectory + "\\extent-config.xml");
+            Instance.AttachReporter(htmlReporter);
+        }
+
+        private ReportHandler()
+        {
+        }
+    }
+}
+
