661f2f18efb1dbfb0f9ec25cf6891bb0612b6d57
Raymond Dasilva <rdasilva@fieldedge.com>
2019-03-25 09:04:39 -0400

    Phone setup logic update. FIXED

--- SeleniumProject/TestCase/Static Tests/FE-5227_Web Static Screen Test Plan for Phone Setup.cs
+++ SeleniumProject/TestCase/Static Tests/FE-5227_Web Static Screen Test Plan for Phone Setup.cs
@@ -30,7 +30,7 @@ namespace SeleniumProject.TestCase.Static_Tests
         [Test, Order(1)]
         public async Task PhoneSetupTest() // FE-5227, test phone setup
         {
-            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
 
             //Go to Phone setup
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#setting-link-container > ul > li > a")));
@@ -50,9 +50,13 @@ namespace SeleniumProject.TestCase.Static_Tests
 
 
             //Phone Setup changes
-            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#TwilioFailoverPhone")));
-            Driver.FindElement(By.CssSelector("#TwilioFailoverPhone")).Click();
-            Driver.FindElement(By.CssSelector("#TwilioFailoverPhone")).SendKeys("1234567890");
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#form")));
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#form > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > label:nth-child(3)")));
+            Driver.FindElement(By.CssSelector("#UseLegacy")).Click();
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#form > div:nth-child(4) > div:nth-child(1)")));
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#TwilioForwardingPhone")));
+            Driver.FindElement(By.CssSelector("#TwilioForwardingPhone")).Click();
+            Driver.FindElement(By.CssSelector("#TwilioForwardingPhone")).SendKeys("1234567890");
             await Task.Delay(5000);
 
             Driver.FindElement(By.CssSelector("button.small:nth-child(1)")).Click();
@@ -67,27 +71,25 @@ namespace SeleniumProject.TestCase.Static_Tests
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#application-menu > div:nth-child(1) > ul > li:nth-child(5) > a > text")));
             Driver.FindElement(By.CssSelector("#application-menu > div:nth-child(1) > ul > li:nth-child(5) > a > text")).Click();
 
-            //verify fallback number has '1234567890' then revert back to empty
-            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#TwilioForwardingPhone")));
+            //verify Forwarding Phone number has '1234567890' then revert back to empty
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#form > div:nth-child(4) > div:nth-child(1)")));
             var phone = Driver.FindElement(By.CssSelector("#TwilioForwardingPhone")).GetAttribute("value");
+           
 
             if (phone == ("1234567890")) // Determines whether update to phone setup are found. Reverts in either case.
             {
 
-                Driver.FindElement(By.CssSelector("#TwilioFailoverPhone")).Click();
-                Driver.FindElement(By.CssSelector("#TwilioFailoverPhone")).Clear();
+                Driver.FindElement(By.CssSelector("#UseLegacy")).Click();
                 Driver.FindElement(By.CssSelector("button.small:nth-child(1)")).Click();
                 
 
             }
             else
             {
-                Driver.FindElement(By.CssSelector("#TwilioFailoverPhone")).Click();
-                Driver.FindElement(By.CssSelector("#TwilioFailoverPhone")).Clear();
+                Driver.FindElement(By.CssSelector("#UseLegacy")).Click();
                 Driver.FindElement(By.CssSelector("button.small:nth-child(1)")).Click();
                 Assert.Fail("FE-5227, Changes were not present. FAIL");
                 await Task.Delay(5000);
-
                 Driver.FindElement(By.CssSelector("")).Click(); //If this comes here, the test did not find saved changes '1234567890'.
                 
             }
