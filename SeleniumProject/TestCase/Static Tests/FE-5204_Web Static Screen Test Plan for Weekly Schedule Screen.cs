c5208fec667fcbcd656e53e8c93f11f9ca03c02d
Delvis Echeverria <decheverria@fieldedge.com>
2019-03-26 10:41:17 -0400

    update 5404 extendreport

--- SeleniumProject/TestCase/Static Tests/FE-5204_Web Static Screen Test Plan for Weekly Schedule Screen.cs
+++ SeleniumProject/TestCase/Static Tests/FE-5204_Web Static Screen Test Plan for Weekly Schedule Screen.cs
@@ -1,8 +1,10 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using SeleniumProject.handler;
 using SeleniumProject.PageObject;
 using System;
 using System.Collections.Generic;
@@ -14,13 +16,27 @@ using SeleniumWaitHelper = SeleniumExtras.WaitHelpers;
 namespace SeleniumProject.TestCase.Static_Tests
 
 {
+    [TestFixture]
     class FE_5204_Web_Static_Screen_Test_Plan_for_Weekly_Schedule_Screen
     {
-        //Selenium Driver
-        protected IWebDriver Driver;
+        
+        IWebDriver Driver = new ChromeDriver();
+        public static ExtentReports extent = null;
+        public static ExtentTest test = null;
 
 
         public object SeleniumExtras { get; private set; }
+        [OneTimeSetUp] //Start extent reporting instance using htmlreporter
+        public void ExtentStart()
+        {
+            ReportHandler.ExtentStart();
+        }
+
+        [OneTimeTearDown]
+        public void ExtentClose()
+        {
+            ReportHandler.ExtentClose();
+        }
 
         [SetUp]  //Nunit - Anotation to execute a method before every test.
         //Start Chrome
@@ -34,8 +50,12 @@ namespace SeleniumProject.TestCase.Static_Tests
         [Test]//Nunit - Anotation to mark a method as a Automated TestCase 
         public async Task WeeklySchedule()
         {
+            try
+            {
+                test = extent.CreateTest("Weekly_Schedule").Info("Test Started"); //Mark start of test.
 
-            HomePage home = new HomePage(Driver);
+
+                HomePage home = new HomePage(Driver);
             home.gotoPage();
 
             await Task.Delay(3000);
@@ -50,7 +70,7 @@ namespace SeleniumProject.TestCase.Static_Tests
 
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#table-btn-container > table > tbody > tr:nth-child(2) > td:nth-child(5) > div.col-lg-9.col-md-8.col-sm-6.col-xs-6 > text")));
             Driver.FindElement(By.CssSelector("#table-btn-container > table > tbody > tr:nth-child(2) > td:nth-child(5) > div.col-lg-9.col-md-8.col-sm-6.col-xs-6 > text")).Click();
-
+                test.Log(Status.Info, "techs are updated.");
 
             //Set Time
 
@@ -61,9 +81,9 @@ namespace SeleniumProject.TestCase.Static_Tests
             //Set a tech "On Call"
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#on-call-container > div")));
             Driver.FindElement(By.CssSelector("#on-call-container > div")).Click();
+                test.Log(Status.Info, "setting time successfully.");
 
-           
-            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#weeklySchedulForm > div:nth-child(2) > button.custom-btn.success-btn.small")));
+                wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#weeklySchedulForm > div:nth-child(2) > button.custom-btn.success-btn.small")));
             Driver.FindElement(By.CssSelector("#weeklySchedulForm > div:nth-child(2) > button.custom-btn.success-btn.small")).Click();
 
             await Task.Delay(3000);
@@ -80,10 +100,19 @@ namespace SeleniumProject.TestCase.Static_Tests
 
          
         }
+
+            catch (Exception e)
+            {
+                test.Log(Status.Fail, e.ToString());
+                throw;
+            }
+        }
+
         [TearDown]
-        public void closeDown()
+        public void CloseDown()
         {
-            Driver.Quit();
+            //Driver.Quit();
+            //test.Log(Status.Info, "FE_5204_Web_Static_Screen_Test_Plan_for_Weekly_Schedule_Screen__PASSED!");
         }
     }
 }
