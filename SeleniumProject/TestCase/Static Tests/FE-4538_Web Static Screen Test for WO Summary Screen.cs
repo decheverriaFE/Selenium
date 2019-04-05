30c18236a0d62faa3b4e75a950440463358ddaef
Raymond Dasilva <rdasilva@fieldedge.com>
2019-04-05 09:18:11 -0400

    Fixes

--- SeleniumProject/TestCase/Static Tests/FE-4538_Web Static Screen Test for WO Summary Screen.cs
+++ SeleniumProject/TestCase/Static Tests/FE-4538_Web Static Screen Test for WO Summary Screen.cs
@@ -36,6 +36,7 @@ namespace SeleniumProject.TestCase.Static_Tests
             WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
             Random randomGenerator = new Random();
             int randomInt = randomGenerator.Next(1000000);
+            int randomSingleInt = randomGenerator.Next(10);
             DateTime today = DateTime.Today;
             var date = today.Date;
 
@@ -46,12 +47,12 @@ namespace SeleniumProject.TestCase.Static_Tests
             await Task.Delay(2000);
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#search")));
             Driver.FindElement(By.CssSelector("#search")).Click();
-            Driver.FindElement(By.CssSelector("#search")).SendKeys("Automation" + Keys.Enter);
+            Driver.FindElement(By.CssSelector("#search")).SendKeys("Automation" + " " + randomSingleInt + Keys.Enter);
             await Task.Delay(2000);
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector(".fixed-body > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1)")));
             Driver.FindElement(By.CssSelector(".fixed-body > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1)")).Click();
 
-            // Work-orders microdashboard --> new Work-order.
+            //Work-orders microdashboard --> new Work-order.
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#details-navigator > div:nth-child(3) > div:nth-child(2)")));
             Driver.FindElement(By.CssSelector("#details-navigator > div:nth-child(3) > div:nth-child(2)")).Click();
             await Task.Delay(1000);
@@ -68,7 +69,11 @@ namespace SeleniumProject.TestCase.Static_Tests
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#taskdivcontainer > span:nth-child(2) > span:nth-child(1)")));
             Driver.FindElement(By.CssSelector("#taskdivcontainer > span:nth-child(2) > span:nth-child(1)")).Click();
             Driver.FindElement(By.CssSelector(".select2-search__field")).SendKeys("HVAC" + Keys.Enter);
-            Driver.FindElement(By.CssSelector("#AddCustomerCallForm > div:nth-child(7) > div:nth-child(1) > div > span > span.selection > span > span.select2-selection__arrow")).Click();
+            Driver.FindElement(By.CssSelector("#leadSourceDivContainer > span > span.selection > span > span.select2-selection__arrow")).Click();
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("body > span > span > span.select2-search.select2-search--dropdown > input")));
+            Driver.FindElement(By.CssSelector("body > span > span > span.select2-search.select2-search--dropdown > input")).SendKeys(Keys.ArrowDown + Keys.Enter);
+
+            Driver.FindElement(By.CssSelector("#select2-primary-tech-dropdown-container")).Click();
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("body > span > span > span.select2-search.select2-search--dropdown > input")));
             Driver.FindElement(By.CssSelector("body > span > span > span.select2-search.select2-search--dropdown > input")).SendKeys("Field T." + Keys.Enter);
             Driver.FindElement(By.CssSelector("#start-date")).SendKeys(date.ToString("MM/dd/yyyy") + Keys.Enter);
@@ -78,25 +83,33 @@ namespace SeleniumProject.TestCase.Static_Tests
             Driver.FindElement(By.CssSelector("button.custom-btn:nth-child(3)")).Click();
             await Task.Delay(6000);
 
-            //Complete work-order -- No show --grab work-order number Writing and very quickly
+            //Complete work-order -- No show
+            await Task.Delay(10000);
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#label")));
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#label > div.clearfix > div.button-container > button.custom-btn.success-btn.save-btn")));            
             Driver.FindElement(By.CssSelector("#label > div.clearfix > div.button-container > button.custom-btn.success-btn.save-btn")).Click();
-            await Task.Delay(4000);
+            await Task.Delay(8000);
+            
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("li.radio:nth-child(13) > label:nth-child(2)")));
             Driver.FindElement(By.CssSelector("li.radio:nth-child(13) > label:nth-child(2)")).Click();
             await Task.Delay(2000);
             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("#saveModal > div:nth-child(2) > div.clearfix.modal-button-container > button.custom-btn.success-btn.modal-confirm-button")));
             Driver.FindElement(By.CssSelector("#saveModal > div:nth-child(2) > div.clearfix.modal-button-container > button.custom-btn.success-btn.modal-confirm-button")).Click();
 
-            /* WO Verification - NEEDS WO LOCATOR
-             wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector("div.note-bundle:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > p:nth-child(1) > a:nth-child(2)")));
-            await Task.Delay(1000);
-            String workOrder1 = Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div/div[1]/div[2]")).GetAttribute("value");
-            String workOrder2 = Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div/div[1]/div[1]")).GetAttribute("value");
-            String workOrder3 = Driver.FindElement(By.CssSelector("div.note-bundle:nth-child(2)")).GetAttribute("value");
+            //Store latest work-order
+            string myWO = Driver.FindElement(By.CssSelector("div.active:nth-child(3)")).Text;
+            await Task.Delay(2000);
 
-            */
+            //Navigate to Work orders list, and ensure you can access this work order
+            Driver.FindElement(By.CssSelector("li.side-bar-icon:nth-child(6) > a:nth-child(1) > span:nth-child(2)")).Click();
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector(".with-units")));
+            await Task.Delay(2000);
+            Driver.FindElement(By.CssSelector("#search")).SendKeys(myWO + Keys.Enter);
+            await Task.Delay(4000);
+            wait.Until(SeleniumWaitHelper.ExpectedConditions.ElementIsVisible(By.CssSelector(".fixed-body > tr:nth-child(1) > td:nth-child(2) > div:nth-child(1)")));
+            Driver.FindElement(By.CssSelector(".fixed-body > tr:nth-child(1) > td:nth-child(2) > div:nth-child(1)")).Click();
+
+
             await Task.Delay(4000);
         }
 
